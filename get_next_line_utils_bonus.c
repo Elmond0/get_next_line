dabf1c812d78340d981ff17db6cfdd1831cac3fc
Elmond0 <emondo1993@gmail.com>
2024-12-31 11:52:23 +0100

    bonus

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,102 @@
+#include "get_next_line_bonus.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	ch;
+
+	ch = (char)c;
+	while (*s)
+	{
+		if (*s == ch)
+			return ((char *)s);
+		s++;
+	}
+	if (ch == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strdup(char *s)
+{
+	size_t	len;
+	size_t	i;
+	char	*str;
+
+	len = ft_strlen(s);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		str[i] = s[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+	int		len1;
+	int		len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (i < len1)
+		str[j++] = s1[i++];
+	free(s1);
+	i = 0;
+	while (i < len2)
+		str[j++] = s2[i++];
+	str[j] = '\0';
+	return (str);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int		i;
+	int		j;
+	char	*str;
+	size_t	max_len;
+
+	i = 0;
+	j = 0;
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	max_len = ft_strlen(s) - start;
+	if (max_len > len)
+		max_len = len;
+	str = (char *)malloc(sizeof(char) * (max_len + 1));
+	if (!str)
+		return (NULL);
+	while (s[i] && i < (char)start)
+		i++;
+	while (j < (int)(max_len))
+	{
+		str[j] = s[i + j];
+		j++;
+	}
+	str[j] = 0;
+	return (str);
+}
